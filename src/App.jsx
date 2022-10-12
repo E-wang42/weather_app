13581637a738ea4a64c4feffb55d39231c77242f
eric <ericwang42@outlook.com>
2022-10-11 23:16:58 -0700

    used a cool hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import { useState, useEffect } from "react";
 import Input from "./components/Input";
 import coldBg from "./assets/cold2.jpg";
 import warmBg from "./assets/warm2.jpg";
+import axios from "axios";
 
 function App() {
+  const [weather, setWeather] = useState("");
+
   const dynamicBg = { backgroundImage: `url(${coldBg})` };
+
+  useEffect(() => {
+    async function getWeather() {
+      const res = await fetch("url");
+      const data = await res.json();
+      setWeather(data);
+    }
+    getWeather();
+  }, []);
 
   return (
     <div className="App" style={dynamicBg}>
